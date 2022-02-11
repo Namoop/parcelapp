c3a817218ec1e22705746fed045525d6d24a8814
theoc <theo1cap@gmail.com>
2022-02-11 13:11:00 -0800

    renamings

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ window["globals"] = [2];
 import "src/types";
 import toml from "config/maps.toml";
 import { cnv, Time, loop } from "./setup";
-import { Button, Sprite } from "./assets/classes/sprite.class";
+import { Button, Sprite } from "./assets/classes/Sprite.class";
 import Bob from "images/bob.png";
 import lmnop from "images/brown.png";
 const app: HTMLElement = document.getElementById("app");
 //https://parceljs.org/features/dependency-resolution/#glob-specifiers
 
-let bob: Sprite, steve: Sprite;
+let bob: Sprite, button: Sprite;
 function init() {
 	app.appendChild(cnv);
 	let img = new Image();
@@ -24,25 +24,26 @@ function init() {
 		console.log("you're welcome");
 	});
 
-	steve = new Button("balloon 12pt", { width: 100 });
-	console.log(steve);
+	button = new Button("balloon 12pt", { width: 100 });
+	console.log(button);
 	let bruh = new Image();
 	bruh.src = lmnop;
-	let ballet = new Sprite(bruh);
-	ballet.move(80, 150);
-	steve.zIndex = 2n
-	let tallet = new Sprite(bruh);
-	tallet.move(90, 180);
+	let square = new Sprite(bruh);
+	square.move(80, 150).resize(100,200)
+	//square.zIndex = 3n
+	button.zIndex = 2n
+	let rect = new Sprite(bruh);
+	rect.move(90, 180);
 
-	//bob.onhover = () => {console.log("cheeeeese")}
+	rect.onhover = () => {console.log("rect hovered")}
 	bob.onclick = () => {
-		console.log("AHHH");
+		console.log("Face clicked");
 	};
 }
 function myloop() {
 	bob.direction += 0.1;
-	steve.move(Math.sin(Date.now() / 3000) * 100 + 200, 200);
-	steve.pointTowards(bob);
+	button.move(Math.sin(Date.now() / 3000) * 100 + 200, 200);
+	button.pointTowards(bob);
 }
 
 init();
